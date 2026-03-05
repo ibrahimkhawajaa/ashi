fbd8ad033548ad1f78b8ab64867b89ac5d3c39ea
ibby <ibrahimmkhawajaa@gmail.com>
2026-03-05 19:33:28 +0500

    vid

--- src/App.jsx
+++ src/App.jsx
@@ -500,8 +500,8 @@ const App = () => {
             loop
             playsInline
             className="w-full h-full object-cover"
-            poster="https://images.unsplash.com/photo-1600585154340-be6161a56a0c?ixlib=rb-4.0.3&auto=format&fit=crop&w=2070&q=80"
-          >
+            
+                     >
             <source src={Vid} type="video/mp4" />
           </video>
           <div className="absolute inset-0 bg-black/50"></div>
